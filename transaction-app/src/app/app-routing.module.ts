de6e04216a1f328679b40aa45cbe8a8a33bde3e8
Dawid <dawid.rozenek@boldare.com>
2020-12-20 14:46:11 +0100

    feature(create-transactions-module): created transaction lazy loaded module with main transactions component

--- transaction-app/src/app/app-routing.module.ts
+++ transaction-app/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'transactions',
+    loadChildren: () => import('./+transactions/transactions.module').then(m => m.TransactionsModule),
+  },
+  {
+    path: '',
+    redirectTo: 'transactions',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'transactions',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
