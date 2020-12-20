87cbb77b0449b0023635d8ffbb8a81e315431a27
Dawid <dawid.rozenek@boldare.com>
2020-12-20 19:54:02 +0100

    feature(create-transaction-module): created transactions module with two basic components, new transaction and transaction list.

---
+++ transaction-app/src/app/+transactions/components/transactions-new/transactions-new.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransactionsNewComponent } from './transactions-new.component';
+
+describe('TransactionsNewComponent', () => {
+  let component: TransactionsNewComponent;
+  let fixture: ComponentFixture<TransactionsNewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TransactionsNewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TransactionsNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- transaction-app/src/app/+transactions/transactions.module.ts
+++ transaction-app/src/app/+transactions/transactions.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionsComponent } from './containers/transactions/transactions.component';
 import { TransactionsRoutingModule } from './transactions-routing.module';
+import { TransactionsListComponent } from './components/transactions-list/transactions-list.component';
+import { TransactionsNewComponent } from './components/transactions-new/transactions-new.component';
 
 @NgModule({
   declarations: [
-    TransactionsComponent
+    TransactionsComponent,
+    TransactionsListComponent,
+    TransactionsNewComponent
   ],
   imports: [
     CommonModule,
